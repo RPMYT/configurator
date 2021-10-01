c216eeac3f9644ffdb041c84155ec5ece5917e25
Prismatic <realprismaticyt@gmail.com>
2021-10-01 17:10:02 +1300

    Small cleanup, we don't need FAPI

--- src/main/java/net/lilydev/configurator/Configurator.java
+++ src/main/java/net/lilydev/configurator/Configurator.java
@@ -1,12 +1,10 @@
 package net.lilydev.configurator;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Configurator implements ModInitializer {
-    public static final String MOD_ID = "configurator";
     public static final Logger LOGGER = LogManager.getLogger("Configurator");
 
     @Override
